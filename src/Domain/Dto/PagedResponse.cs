c09ac16aadb51c8b1d33bc5593bb9f9d35639144
Fehmi Anaç <fehmianac@gmail.com>
2023-10-20 16:02:52 +0300

    AwsWebSocketAdapterConfig SQS added. PagedResponse fixed.

--- src/Domain/Dto/PagedResponse.cs
+++ src/Domain/Dto/PagedResponse.cs
@@ -3,7 +3,15 @@ namespace Domain.Dto
     public class PagedResponse<T>
     {
         public IList<T> Data { get; set; } = default!;
-        public string? NextToken { get; set; }
+
+        private string? _nextToken;
+
+        public string? NextToken
+        {
+            get => _nextToken;
+            set => _nextToken = value != "%7b%7d" ? value : null;
+        }
+
         public int Limit { get; set; }
         public string? PreviousToken { get; set; }
     }
