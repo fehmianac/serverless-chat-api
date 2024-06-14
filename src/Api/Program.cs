091a99ffdc298658958453afa7c29332293a672d
Fehmi Anaç <fehmianac@gmail.com>
2024-06-14 10:12:32 +0300

    swagger operation ids.

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -55,7 +55,28 @@ builder.Logging.AddSerilog(logger);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.CustomOperationIds(e =>
+    {
+        // Extract and join route values
+        var routeValues = string.Join(
+            "_",
+            e.ActionDescriptor.RouteValues.OrderByDescending(o => o.Key)
+                .Select(i => i.Value)
+        );
+
+        // Extract the namespace from the MethodInfo in EndpointMetadata
+        var methodInfo = e
+            .ActionDescriptor.EndpointMetadata.OfType<MethodInfo>()
+            .FirstOrDefault();
+        var namespaceName =
+            methodInfo?.DeclaringType?.Namespace?.Split('.').Last() ?? "Default";
+
+        // Return the custom operation ID including the namespace and route values
+        return $"{namespaceName}_{routeValues}";
+    });
+});
 
 var option = builder.Configuration.GetAWSOptions();
 builder.Services.AddDefaultAWSOptions(option);
